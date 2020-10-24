8c9118a5ddea8a1f93d1da0efb3cc67b2039cc97
ナターシャ <nc@MBP-de-N.lan>
2020-10-24 17:32:56 +0200

    adding files

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.febd05622c78099b6bf7800aae443f57.js"
+  "/precache-manifest.59a73586c407bc04037d3c2df7eb254e.js"
 );
 
 self.addEventListener('message', (event) => {
