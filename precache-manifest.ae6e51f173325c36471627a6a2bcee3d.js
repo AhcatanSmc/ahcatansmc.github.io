a2a1bb83026f1c09f967611a12b7ea12ce0c11f2
ナターシャ <nc@MBP-de-N.lan>
2020-10-24 16:46:42 +0200

    Updates

--- precache-manifest.ae6e51f173325c36471627a6a2bcee3d.js
+++ precache-manifest.ae6e51f173325c36471627a6a2bcee3d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f38f31e24ba740768fb2eb460d8e6c4",
+    "revision": "a80389f2b6b2bb66d0ab72728261ca3b",
     "url": "/index.html"
   },
   {
-    "revision": "a6e88b863de49b2838d5",
-    "url": "/static/css/main.e6e8fa21.chunk.css"
+    "revision": "a93db8cec32e73530a50",
+    "url": "/static/css/main.9292dc52.chunk.css"
   },
   {
     "revision": "7bf47087c051da962f2c",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.03b9b172.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a6e88b863de49b2838d5",
-    "url": "/static/js/main.b8bab32d.chunk.js"
+    "revision": "a93db8cec32e73530a50",
+    "url": "/static/js/main.00e4d8c0.chunk.js"
   },
   {
     "revision": "b63a4b8ca5166987a4a7",
