8c79017fefece0677bc79f25e4fc8d37d453358c
ナターシャ <nc@MBP-de-N.lan>
2020-10-24 15:22:08 +0200

    Updates

---
+++ precache-manifest.d2b96448c981650528442214ad4bc4de.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1498ec3086a85c1f588104f8bd863d97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d83427ac6d7ab85fc4fa",
+    "url": "/static/css/main.62aca588.chunk.css"
+  },
+  {
+    "revision": "7bf47087c051da962f2c",
+    "url": "/static/js/2.03b9b172.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.03b9b172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d83427ac6d7ab85fc4fa",
+    "url": "/static/js/main.e1114e76.chunk.js"
+  },
+  {
+    "revision": "b63a4b8ca5166987a4a7",
+    "url": "/static/js/runtime-main.2ca0876f.js"
+  },
+  {
+    "revision": "e27d0ca01446bf437b2b4d65662e9faa",
+    "url": "/static/media/Background.e27d0ca0.jpg"
+  },
+  {
+    "revision": "a2004c82a18c2f87d7646204fbd900e1",
+    "url": "/static/media/github.a2004c82.png"
+  },
+  {
+    "revision": "6edd621a8d44fa21594f8db3028fb82c",
+    "url": "/static/media/insta.6edd621a.png"
+  },
+  {
+    "revision": "4adc3fae4c095a07006ea38a8829074e",
+    "url": "/static/media/linkedin.4adc3fae.png"
+  },
+  {
+    "revision": "dad75ebdb8ca74fd7d452cb952875bdf",
+    "url": "/static/media/mail.dad75ebd.png"
+  },
+  {
+    "revision": "f6422f03178d60c233f28795c41cc1fa",
+    "url": "/static/media/ncpicture.f6422f03.jpg"
+  }
+]);
