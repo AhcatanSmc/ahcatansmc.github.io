6a62f20eac50df625ee838b80a62f9a75c929a82
ナターシャ <nc@MBP-de-N.lan>
2020-10-26 10:31:12 +0100

    adding files

---
+++ build/precache-manifest.934783aed69f8246e7172b91b2e08280.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d69c6a96173fda5a734c875274121be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcf8668407ffe14e5839",
+    "url": "/static/css/main.7a7ee950.chunk.css"
+  },
+  {
+    "revision": "fc0a7f7152b8a7e2f798",
+    "url": "/static/js/2.5591b868.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/static/js/2.5591b868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcf8668407ffe14e5839",
+    "url": "/static/js/main.01d66f87.chunk.js"
+  },
+  {
+    "revision": "b63a4b8ca5166987a4a7",
+    "url": "/static/js/runtime-main.2ca0876f.js"
+  },
+  {
+    "revision": "56b9c1b96e57dd8b02c5cdc5a5bfe938",
+    "url": "/static/media/Background.56b9c1b9.jpg"
+  },
+  {
+    "revision": "eb636db8e48dbed74c61af1da6fc3486",
+    "url": "/static/media/ncpicture.eb636db8.jpg"
+  }
+]);
